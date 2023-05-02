043b3b27eac3a1e273cfe2783b79bc07e221273a
pjayaprakash1510 <118723199+pjayaprakash1510@users.noreply.github.com>
2023-05-02 14:37:51 +0530

    Update ProfileComponent.js

--- portfolio/src/client/components/ProfileComponent.js
+++ portfolio/src/client/components/ProfileComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../css/index.css";
 
-import profile from "../images/ic_profile_img.jpg";
+import profile from "D:\srms\images\profile.png";
 import facebook from "../images/ic_facebook.png";
 import twitter from "../images/ic_twitter.png";
 import github from "../images/ic_github.png";
@@ -25,15 +25,15 @@ class ProfileComponent extends React.Component {
                     <div className={'dashboard-profile-img-container'}>
                         <img src={profile} className={'dashboard-profile-img'} />
                         <div className={'social-profile-container'}>
-                            <a href="https://www.facebook.com/hackbotone/" target="_blank"><img src={facebook} className={'social-profile-icon'} /></a>
-                            <a href="https://twitter.com/anspattnaik" target="_blank"><img src={twitter} className={'social-profile-icon'} /></a>
-                            <a href="https://www.linkedin.com/in/anshuman123/" target="_blank"><img src={linkedin} className={'social-profile-icon'} /></a>
-                            <a href="https://github.com/anshumanpattnaik" target="_blank"><img src={github} className={'social-profile-icon'} /></a>
-                            <a href="https://www.youtube.com/anshumanpattnaik" target="_blank"><img src={youtube} className={'social-profile-icon'} /></a>
+                            <a href="https://www.facebook.com" target="_blank"><img src={facebook} className={'social-profile-icon'} /></a>
+                            <a href="https://twitter.com" target="_blank"><img src={twitter} className={'social-profile-icon'} /></a>
+                            <a href="https://www.linkedin.com" target="_blank"><img src={linkedin} className={'social-profile-icon'} /></a>
+                            <a href="https://github.com" target="_blank"><img src={github} className={'social-profile-icon'} /></a>
+                            <a href="https://www.youtube.com" target="_blank"><img src={youtube} className={'social-profile-icon'} /></a>
                         </div>
                     </div>
                     <div className={'dashboard-profile-details-container'}>
-                        <p className={'dashboard-profile-name-label'}>Hello, I'm <span className={'dashboard-profile-name'}>Anshuman Pattnaik</span></p>
+                        <p className={'dashboard-profile-name-label'}>Hello, I'm <span className={'dashboard-profile-name'}>JAYAPRAKASH P</span></p>
                         <p className={'dashboard-profile-deatils'}>
                             I'm a Senior Software Engineer specialized in Android & Web application development, having fluent knowledge in the Android framework and developed many mobile applications from various domains such as (Payment, IoT, Graphics, Augmented Reality & Virtual Reality) and also developed cross-platform based mobile application using React-Native framework. In terms of the Web framework having experience in both back-end and front-end development, developed many full-stack based web application using Node.js, React.js & MongoDB.
                             </p>
